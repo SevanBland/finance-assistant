b3f31ea1b59e8930489c5a6c825a46400ae1c45b
SevanBland <sevanbland@gmail.com>
2026-06-15 20:50:32 -0400

    Add hello endpoint

---
+++ src/main/java/com/sevanbland/finance_assistant/HelloController.java
@@ -0,0 +1,12 @@
+package com.sevanbland.finance_assistant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from finance-assistant!";
+    }
+}
